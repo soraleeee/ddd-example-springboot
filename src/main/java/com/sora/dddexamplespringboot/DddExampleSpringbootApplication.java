64a17ed18830723474166542f8e4c19817ead3fc
이소라(SoRa Lee) <oic_b93502@ocic.co.kr>
2023-03-26 20:58:46 +0900

    Init

---
+++ src/main/java/com/sora/dddexamplespringboot/DddExampleSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.sora.dddexamplespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddExampleSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DddExampleSpringbootApplication.class, args);
+	}
+
+}
